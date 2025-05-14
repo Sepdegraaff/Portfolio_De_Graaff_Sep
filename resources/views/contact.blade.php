0df59e7ef27eb53245603a0007cc94d23bbb29a1
SepdeGraaff <sep.de.graaff@maxserv.com>
2025-05-14 16:32:35 +0200

    Whoops

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
     @include('layouts.header')
-    <section class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <section class="pb-[250px] px-6 grid grid-cols-1 md:grid-cols-2 gap-4">
         <div class="flex flex-col gap-4 items-center hidden-animation transition-all duration-700">
-            <img class="object-contain max-w-[500px] min-h-full" src="{{ asset('images/Profile_Picture.png') }}" alt="face">
+            <img class="object-contain w-full max-w-[500px]" src="{{ asset('images/Profile_Picture.png') }}" alt="face">
         </div>
         <div class="hidden-animation flex flex-col gap-4 transition-all duration-700">
-            <h2 class="text-5xl">
+            <h2 class="text-3xl md:text-5xl ">
                 Contactgegevens
             </h2>
             <div class="flex flex-col gap-6 font-light text-xl">
